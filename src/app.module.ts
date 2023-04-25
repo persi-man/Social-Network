6ad858e284c5ebe642c38ccb61bc66b2e1303c3a
djebril <djeben91@gmail.com>
2023-04-25 16:31:03 +0200

    update_bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CommentService } from './video_and_picture/comment/comment.service';
-import { CommentService } from './video_and_picture/comment/controller/comment/comment.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, CommentService],
+  providers: [AppService],
 })
 export class AppModule {}
